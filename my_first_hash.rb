8b169fce3fb6927a496c82a1c876dbbabba959ac
David Bermudez <juan.bermudez1102@outlook.com>
2019-10-09 16:31:21 +0000

    Automatically backed up by Learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -1,7 +1,5 @@
 def my_hash
-  # use the literal constructor to set the variable, my_hash, equal to a hash with key/value pairs of your choice.
-
-  
+  hash = { "David" => {:age => 23, :height => 6}, "Ron" => {:age => 28, :height => 6.1}}
 end
 
 
